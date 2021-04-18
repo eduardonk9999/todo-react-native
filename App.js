5c24052856d7b28641cb0726df799ac8dc1bb783
eduardonk9999 <thiago.novak7@gmail.com>
2021-04-18 15:49:33 -0300

    Add, final app

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableWithoutFeedback, FlatList } from 'react-native';
 
 export default function App() {
+  const [task, updateTask] = useState('');
+  const [tasks, updateTasks] = useState([]);
+
+  const handleAdd = () => {
+    if (task.trim()) {
+      updateTasks([... tasks, task]);
+      updateTask('');
+    }
+  }
+
+  const handleTask = ({ item }) => <Text style={styles.item}>{item}</Text>
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>To-do List</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.field}
+          autoCapitalize="none"
+          autoCompleteType="off"
+          autoCorrect={false}
+          placeholder="Insira um todo"
+          onChangeText={text => updateTask(text)}
+          value={task}
+        />
+
+        <TouchableWithoutFeedback onPress={handleAdd}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Add</Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+
+      <FlatList data={tasks} keyExtractor={item => item} renderItem={handleTask} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +45,43 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    padding: 30,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 25,
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  field: {
+    borderWidth: 1,
+    borderColor: '#dcdcdc',
+    padding: 10,
+    fontSize: 15,
+    color: '#333',
+    borderRadius: 5,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    marginRight: 10,
+  },
+  button: {
+    backgroundColor: '#00cc99',
+    padding: 5,
+    borderRadius: 5,
     justifyContent: 'center',
   },
+  buttonText: {
+    color: '#fdfdfd'
+  },
+  item: {
+    borderWidth: 1,
+    borderColor: '#dcdcdc',
+    padding: 10,
+    marginTop: 15,
+    borderRadius: 5,
+
+  },
+  form: {
+    flexDirection: 'row'
+  },
+
 });
